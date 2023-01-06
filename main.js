6eb171bfd315ae38e1d5b842bba49dafdf1f7a14
laura-williams-1 <laurawilliams@pursuit.org>
2023-01-05 20:54:55 -0500

    4/4 test

--- main.js
+++ main.js
@@ -1,14 +1,18 @@
 const allPost = document.querySelector("li");
+console.log(allPost);
 allPost.remove();
+// - [ ] Remove the "All Posts" link from the header.
+// - [ ] Remove the `span` element that contains the "Read Time" in the featured post. The featured post is the first post on the page.
+// - [ ] Remove the last post from the page, titled "Stop Planning".
+// - [ ] Remove all titles from all non-featured posts.
 
 const readTime = document.querySelector("span");
-
 readTime.remove();
 
-const lastPost = document.querySelector(" main .posts article");
+const posts = document.querySelectorAll("article");
+console.log(posts);
+posts[posts.length - 1].remove();
 
-lastPost.remove();
+const titles1 = document.querySelectorAll("h3");
 
-const titles = document.querySelectorAll("h3");
-
-titles.forEach((title) => title.remove(h3));
+titles1.forEach((title) => title.remove());
